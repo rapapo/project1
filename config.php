7c50f4da89190974afe15ae2ddc135474634d923
rapapo <asyraf.akif91k@gmail.com>
2022-06-16 17:05:10 +0800

    1

---
+++ config.php
@@ -0,0 +1,19 @@
+config.php
+
+<?php
+
+   // Database configuration    
+   $hostname = "localhost"; 
+   $username = "root"; 
+   $password = ""; 
+   $dbname   = "project2";
+    
+   // Create database connection 
+   $con = new mysqli($hostname, $username, $password, $dbname); 
+    
+   // Check connection 
+   if ($con->connect_error) { 
+       die("Connection failed: " . $con->connect_error); 
+   }
+
+?>
