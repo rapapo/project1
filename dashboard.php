80e563aaed97bfb65bbf709f20c853a29c17859e
rapapo <asyraf.akif91k@gmail.com>
2022-06-16 17:28:42 +0800

    3

--- dashboard.php
+++ dashboard.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION['ID'])) {
     </head>
        <body>
        <nav class="navbar navbar-info sticky-top bg-info flex-md-nowrap p-10">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="" style="color: #5b5757;"><b>Webs Codex</b></a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="" style="color: #5b5757;"><b>UITM</b></a>
                <ul class="navbar-nav px-3">
          <li class="nav-item text-nowrap">
                  <a class="nav-link" href="logout.php">Hi, <?php echo ucwords($_SESSION['NAME']); ?> Log out</a>
