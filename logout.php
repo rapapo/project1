12c17cc2896f8c81bc31877db691fc0d8c7eabcb
rapapo <asyraf.akif91k@gmail.com>
2022-06-16 17:15:37 +0800

    1

---
+++ logout.php
@@ -0,0 +1,11 @@
+logout.php
+
+<?php
+   session_start();
+   session_destroy();
+   session_unset($_SESSION['NAME']);
+   session_unset($_SESSION['ROLE']);
+   session_unset($_SESSION['ID']);
+   header("Location:login.php");
+   die();
+?>
